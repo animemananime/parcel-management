b58a90da582acbf2c277ed7606d4c0d8fccc4afb
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-12-04 22:57:03 +0800

    remove

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 var router = require("express").Router();
 
 // split up route handling
-router.use("/parcel", require("./parcel"));
+// router.use("/parcel", require("./parcel"));
 router.get('/ping', (req, res) => {
     res.send('Everything is pinging.')
   })
